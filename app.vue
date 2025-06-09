415fdb91bb07c9ca1219f4b80c6d8694b8374c4a
Peter Pike <pikepeter@gmail.com>
2025-06-09 11:23:17 +0400

    :sparkles: add landing page

--- app.vue
+++ app.vue
@@ -1,8 +1,5 @@
 <template>
-  <div>
-    hello world
-  </div>
-  <button class="btn btn-secondary">
-    View Map <Icon name="tabler:map" size="32" />
-  </button>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
