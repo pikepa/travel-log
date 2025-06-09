5bd475b23ccf1de61eb2ea7b1561d7154dd0cf9e
Peter Pike <pikepa@users.noreply.github.com>
2025-06-09 17:56:03 +0400

    added auth migrations, modified existing tables to include user_id (#55)

--- lib/db/schema/location-log-image.ts
+++ lib/db/schema/location-log-image.ts
@@ -1,5 +1,6 @@
 import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
+import { user } from "./auth";
 import { locationLog } from "./location-log";
 
 export const locationLogImage = sqliteTable("locationLogImage", {
@@ -7,6 +8,7 @@ export const locationLogImage = sqliteTable("locationLogImage", {
   key: text().notNull(),
   description: text(),
   locationLogId: int().notNull().references(() => locationLog.id),
+  userId: int().notNull().references(() => user.id, { onDelete: "cascade" }),
   createdAt: int().notNull().$default(() => Date.now()),
   updatedAt: int().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()),
 });
