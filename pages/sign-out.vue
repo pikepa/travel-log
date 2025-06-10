df53017acc1bcf48b6e4f059651dba9e5162a011
Peter Pike <pikepa@users.noreply.github.com>
2025-06-10 17:20:21 +0400

    :Sparkes: adds midleware to prevent guests accessing anything within the dashboard directory' (#57)

--- pages/sign-out.vue
+++ pages/sign-out.vue
@@ -1,13 +1,26 @@
 <script lang="ts" setup>
 const authStore = useAuthStore();
-
-onMounted(() => {
-  authStore.signOut();
-});
 </script>
 
 <template>
-  <div class="card bg-base-300 container mt-4 min-h-72 text-center mx-auto flex flex-col justify-center items-center gap-4">
-    <span class="loading loading-spinner loading-xl" />
+  <div class="hero bg-base-300 container mx-auto mt-4">
+    <div class="hero-content text-center min-h-96">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Travel Log
+        </h1>
+        <p class="py-6">
+          Keep track of your travels and adventures with this simple travel log app. Add locations, photos, and notes to create a digital journal of your journeys.
+        </p>
+        <AuthButton v-if="!authStore.user" />
+        <NuxtLink
+          v-else
+          to="/dashboard"
+          class="btn btn-primary"
+        >
+          Start Logging
+        </NuxtLink>
+      </div>
+    </div>
   </div>
 </template>
